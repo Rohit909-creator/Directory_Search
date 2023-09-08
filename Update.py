c8a0f1cebc76a87a93f2a053bb5738ca33c7e5a8
ROHIT FRANCIS <108980398+Rohit909-creator@users.noreply.github.com>
2023-09-09 04:30:42 +0530

    Update Update.py

--- Update.py
+++ Update.py
@@ -4,8 +4,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import json
 openai.api_key = os.environ['API_KEY']
 
-path = 'C:\\Users\\ROHIT FRANCIS\\OneDrive\\Desktop\\ALL Here\\AI_Ideas\\Directory Search'
-
+# path = 'C:\\Users\\ROHIT FRANCIS\\OneDrive\\Desktop\\ALL Here\\AI_Ideas\\Directory Search'
+path = "C:\\Users\\ROHIT FRANCIS\\OneDrive\\Desktop\\ALL Here\\AI_Ideas\\DirectorySearch_2_0"
 
 class Search():
 
@@ -19,16 +19,16 @@ class Search():
 
         keys = list(d.keys())
         keys = keys[3:]
-
-        for key in keys:
+        self.keys = keys
+        for key in self.keys:
             self.embeddings.append(d[key])
-        
+        # print(self.keys)
     def query(self, query):
 
         embs = self.get_embedding(query)
         probs = cosine_similarity([embs], self.embeddings)
         idx = probs.argmax(axis=-1)
-        return probs, idx
+        return probs, idx, self.keys[idx.item()]
 
     def get_embedding(self,text, model="text-embedding-ada-002"):
         text = text.replace("\n", " ")
@@ -45,18 +45,21 @@ class Search():
             s = json.dumps(d)
             f.write(s)
 
+        self.embeddings.append(embs)
+        self.keys.append(key)
 if __name__ == '__main__':
     s = Search()
     # embs = s.get_embedding('hello how are you?')
     # print(len(embs))
-    s.update([1,2],"1")
-    s.update([1,2],"3")
-    probs, idx = s.query("a folder named hi")
-    print(probs, idx)
+    # s.update([1,2],"1")
+    # s.update([1,2],"3")
+    probs, idx, path_ = s.query("AI projects by Rohit Francis")
+    print(probs, idx, path_)
     with open(path+"\\Dir_Embed_data.json","r") as f:
         s = f.read()
         d = json.loads(s)
-    print(d.keys())
+    
+    # print(f'\n\n\n{d.keys()}')
     # print(len(d[list(d.keys())[-1]]))
     
             
